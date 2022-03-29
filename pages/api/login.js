2f7154585cf7408ba5bf41134558cc6f9648cd87
ulysses <atkesonu@gmail.com>
2022-03-29 11:48:22 -0500

    begin key expose

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,6 +1,7 @@
 import { setAuthCookie } from "src/lib/cookie";
 import { generateAccessToken } from "src/lib/jwt";
 import { prisma } from "src/lib/prisma";
+import { crypto } from "crypto";
 
 export default async function handler(req, res) {
   if (req.method == "POST") {
@@ -20,6 +21,7 @@ export default async function handler(req, res) {
           email: email,
           username: email?.match(/^([^@]*)@/)[1],
           balance: 100,
+          api_key: crypto.randomBytes(16).toString("hex"),
         },
       });
     }
