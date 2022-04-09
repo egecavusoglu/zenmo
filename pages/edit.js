d8a45edfbe71630bddd832002dce508c44ce6f90
ulysses <atkesonu@gmail.com>
2022-04-09 00:17:10 -0400

    add edit page

---
+++ pages/edit.js
@@ -0,0 +1,80 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+  HStack,
+  Avatar,
+  AvatarBadge,
+  IconButton,
+  Center,
+} from "@chakra-ui/react";
+
+export default function UserProfileEdit() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("a");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        <Stack
+          spacing={4}
+          w={"full"}
+          maxW={"md"}
+          bg={useColorModeValue("white", "gray.700")}
+          rounded={"xl"}
+          boxShadow={"lg"}
+          p={6}
+          my={12}
+        >
+          <Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
+            User Profile Edit
+          </Heading>
+
+          <FormControl id="userName" isRequired>
+            <FormLabel>User name</FormLabel>
+            <Input
+              placeholder="User name"
+              _placeholder={{ color: "gray.500" }}
+              type="text"
+            />
+          </FormControl>
+
+          <Stack spacing={6} direction={["column", "row"]}>
+            <Button
+              bg={"red.400"}
+              color={"white"}
+              w="full"
+              _hover={{
+                bg: "red.500",
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              bg={"blue.400"}
+              color={"white"}
+              w="full"
+              type="submit"
+              _hover={{
+                bg: "blue.500",
+              }}
+            >
+              Submit
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+    </form>
+  );
+}
