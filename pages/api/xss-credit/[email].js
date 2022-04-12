313c2b184d0a23fa22dccf3dcfc33f23a7fb4bdc
Ege Cavusoglu <egecavusoglu99@gmail.com>
2022-04-12 12:45:41 -0500

    Add xss credit endpoint

---
+++ pages/api/xss-credit/[email].js
@@ -0,0 +1,39 @@
+import { prisma } from "src/lib/prisma";
+import { authenticationMiddleware } from "src/lib/jwt";
+import { runMiddleware } from "src/lib/middleware";
+import { getAuthCookie } from "src/lib/cookie";
+import { Vulnerabilities } from "@prisma/client";
+import { TASK_COMPLETED_STATUS } from "src/lib/fetch";
+
+/**
+ * This endpoint is used to fetch user's profile data.
+ */
+export default async function handler(req, res) {
+  let { email } = req.query;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+
+  const id = user.id;
+
+  await prisma.task.upsert({
+    where: {
+      vulnerability_userId: {
+        userId: id,
+        vulnerability: Vulnerabilities.XSS_ATTACK,
+      },
+    },
+    update: {},
+    create: {
+      vulnerability: Vulnerabilities.XSS_ATTACK,
+      userId: id,
+    },
+  });
+
+  res.status(200).json({
+    isSuccess: true,
+  });
+}
